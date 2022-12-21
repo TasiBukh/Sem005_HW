882b4c28c25d0b95bc290baf1efcc611824a656e
Anastasia B <nastja.bukhanova@gmail.com>
2022-12-21 14:34:51 +0300

    task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+Console.WriteLine($"Количество элементов массива: ");
+int numberElements = int.Parse(Console.ReadLine());
+
+int RandomNumbers(int numberElements, int min, int max)
+{
+    int[] randomNumbers = new int[numberElements];
+    int sumElements = 0;
+    Console.WriteLine("Массив: ");
+
+    for (int i =0; i < randomNumbers.Length; i++)
+    {
+        randomNumbers[i] = new Random().Next(min, max);
+        Console.WriteLine(randomNumbers[i] + " ");
+
+        if (i % 2 != 1)
+        {
+            sumElements = sumElements + randomNumbers[i];
+        }
+    }
+    return sumElements;
+}
+
+int randomNumbers = RandomNumbers(numberElements, 1, 10);
+Console.WriteLine($"Сумма элементов на нечетных индексах: {randomNumbers}");
